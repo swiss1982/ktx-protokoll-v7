a326568e2b282db7982eb0f2431e45c482e33da6
swiss1982 <Romanwanczura@hotmail.com>
2026-04-17 05:06:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,43 @@
-self.addEventListener('install', () => {
+const CACHE_NAME = "kanaltech-v1";
+
+const urlsToCache = [
+  "/ktx-protokoll-v7/",
+  "/ktx-protokoll-v7/index.html",
+  "/ktx-protokoll-v7/manifest.json",
+  "/ktx-protokoll-v7/file_000000001a1c7243a0ca80606ae181fa.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', () => {
-  // nichts machen → kein cache
+// Fetch (Turbo)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
